0728638a48b305e191cbb1d0a59e1168fb12173a
dragonsky2357 <qkrdydals327@gmail.com>
2024-02-02 17:16:11 +0900

    feat: update ScheduleRepository

--- src/main/java/com/dragonsky/schedule/repository/ScheduleRepository.java
+++ src/main/java/com/dragonsky/schedule/repository/ScheduleRepository.java
@@ -6,9 +6,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 
 public interface ScheduleRepository extends JpaRepository<Schedule,Long> {
     List<Schedule> findByMember(Member member);
+
+    Optional<Schedule> findByMemberAndId(Member member, Long id);
     List<Schedule> findByMemberOrderByCreateAtDesc(Member member);
+    List<Schedule> findByMemberAndDoneIsFalseOrderByCreateAtDesc(Member member);
+    List<Schedule> findByMemberAndTitleContainingOrderByCreateAtDesc(Member member,String title);
 
 }
